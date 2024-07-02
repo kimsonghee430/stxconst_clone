0b9f454545d3213beb45637769faae91eef1cb6e
Kim Songhee <bokchi8@gmail.com>
2024-07-02 11:17:43 +0900

    css 모달창 완성 / javascript 모달창 설정 완료

--- js/main.js
+++ js/main.js
@@ -46,7 +46,34 @@ window.onload = function () {
         topBtnImg.classList.remove("up")
       }
     });
-  
+  // 모달창 부분=====================
+    const body = document.querySelector("body")
+    const modal = document.querySelector(".modal-wrap")
+    const modalClose = document.querySelector(".modal-close")
+    // isOpen 이라는 값에 따라 스크롤을 제어하는 함수
+    function controlScroll(isOpen){
+      if(isOpen){
+        body.style.overflow = "hidden"; /*오버플로우는 스크롤이라는 의미*/
+      }
+      else{
+        body.style.overflow = "auto";
+      }
+    }
+
+    
+    
+    // 초기 모달창 상태 설정
+    const isOpen = true;
+    controlScroll(isOpen); /*위에서 기능하려면 여기서 한번 불러줘야 함*/
+    
+    modalClose.addEventListener("click",function(){
+      modal.style.display = "none";
+      // 모달창이 닫힐 때는 스크롤을 다시 활성화시킴
+      controlScroll(false);
+    });
+    
+
+
   };
   
   
